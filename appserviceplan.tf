cd1813307724afa69f1b364db6afa467e9015e3c
OklenCodes <oklen.blake@hotmail.co.uk>
2024-08-27 18:07:16 +0100

    Update appserviceplan.tf

--- appserviceplan.tf
+++ appserviceplan.tf
@@ -3,7 +3,10 @@ resource "azurerm_service_plan" "frontend-asp" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "P1v2"   # Upgrade to a SKU that supports Availability Zones
+  zone_balancing_enabled = true  # Enable zone redundancy
+  per_site_scaling    = false
+
   depends_on = [
     azurerm_subnet.frontend-subnet
   ]
@@ -14,7 +17,10 @@ resource "azurerm_service_plan" "backend-asp" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "P1v2"   # premium app service plan sku supports Availability Zones
+  zone_balancing_enabled = true  # Enable zone redundancy
+  per_site_scaling    = false
+
   depends_on = [
     azurerm_subnet.backend-subnet
   ]
