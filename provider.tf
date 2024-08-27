415215b063bdff51209623141b34a0ff61b1c160
OklenCodes <oklen.blake@hotmail.co.uk>
2024-08-27 15:11:22 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,16 +5,13 @@ terraform {
       version = "~> 4.0.1"
     }
   }
-  # here i am storing the state file 
+  # Storing the state file here
   backend "azurerm" {
     resource_group_name  = "myTFResourceGroup"
     storage_account_name = "statestoragetf"
-    container_name       = "statecon"
     key                  = "terraform.tfstate"
   }
 }
-
-
 provider "azurerm" {
   features {}
 }
