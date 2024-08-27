741254a12a7299c2bcb1d39b1c66f9bef25a4e22
OklenCodes <oklen.blake@hotmail.co.uk>
2024-08-27 15:12:09 +0100

    Update appserviceplan.tf

--- appserviceplan.tf
+++ appserviceplan.tf
@@ -1,4 +1,4 @@
-resource "azurerm_service_plan" "fe-asp" {
+resource "azurerm_service_plan" "frontend-asp" {
   name                = "fe-asp-prod"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
@@ -9,9 +9,7 @@ resource "azurerm_service_plan" "fe-asp" {
   ]
 }
 
-
-
-resource "azurerm_service_plan" "be-asp" {
+resource "azurerm_service_plan" "backend-asp" {
   name                = "be-asp-prod"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
@@ -20,17 +18,4 @@ resource "azurerm_service_plan" "be-asp" {
   depends_on = [
     azurerm_subnet.be-subnet
   ]
-}
------
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
-resource "azurerm_service_plan" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  os_type             = "Linux"
-  sku_name            = "P1v2"
 }
