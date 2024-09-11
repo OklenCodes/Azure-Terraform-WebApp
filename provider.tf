906f29fca8e3cfe093b8bd9404a21fa31e399ea4
OklenCodes <oklen.blake@hotmail.co.uk>
2024-09-11 19:25:34 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,13 +5,26 @@ terraform {
       version = "~> 4.0.1"
     }
   }
-  # Storing the state file here
-  backend "azurerm" {
-    resource_group_name  = "myTFResourceGroup"
-    storage_account_name = "statestoragetf"
-    key                  = "terraform.tfstate"
-  }
 }
+
+  # Storing the state file here
+resource "azurerm_storage_account" "state_account" {
+  name                     = "oklenstorageaccount"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  }
+
+resource "azurerm_storage_container" "state" {
+  name                  = "oklenstatecontainer"
+  storage_account_name  = azurerm_storage_account.state_account.name
+  container_access_type = "private"
+}
+
+
 provider "azurerm" {
   features {}
+
+  subscription_id = "2aa9990f-7dbb-42de-8598-4164d715d5e8"  # Hide this as good practise
 }
