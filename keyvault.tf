a15f49303bf5da2a74de06b07798d713d33ffd98
OklenCodes <oklen.blake@hotmail.co.uk>
2024-09-14 20:11:46 +0100

    Update keyvault.tf

--- keyvault.tf
+++ keyvault.tf
@@ -2,8 +2,8 @@ data "azurerm_client_config" "current" {}  #Use to access the configuration of t
 
 
 
-resource "azurerm_key_vault" "fg-keyvault" {
-  name                        = "fgkeyvaulttest"
+resource "azurerm_key_vault" "ok-keyvault" {
+  name                        = "okkeyvaulttest"
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true
@@ -17,7 +17,7 @@ resource "azurerm_key_vault" "fg-keyvault" {
 # this permission is for service connection from app registration, this is given to store database secrets to key vault
 resource "azurerm_key_vault_access_policy" "kv_access_policy_sc" {
 
-  key_vault_id = azurerm_key_vault.fg-keyvault.id
+  key_vault_id = azurerm_key_vault.ok-keyvault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = "54b84d5e-84a5-49fc-8c76-f8d0604cc8c1"
   key_permissions = [
@@ -27,24 +27,24 @@ resource "azurerm_key_vault_access_policy" "kv_access_policy_sc" {
     "Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Set"
   ]
 
-  depends_on = [azurerm_key_vault.fg-keyvault]
+  depends_on = [azurerm_key_vault.ok-keyvault]
 }
 
 # permission to my self
 resource "azurerm_key_vault_access_policy" "kv_access_policy_me" {
-  key_vault_id       = azurerm_key_vault.fg-keyvault.id
+  key_vault_id       = azurerm_key_vault.ok-keyvault.id
   tenant_id          = data.azurerm_client_config.current.tenant_id
   object_id          = "ab35c899-90de-4dcc-9d4f-ebae7a569976"
   key_permissions    = ["Get", "List"]
   secret_permissions = ["Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Set"]
 
-  depends_on = [azurerm_key_vault.fg-keyvault]
+  depends_on = [azurerm_key_vault.ok-keyvault]
 }
 
 
 # Retrieve the managed identity's principal ID (object_id)
 resource "azurerm_key_vault_access_policy" "kv_access_policy_web_app" {
-  key_vault_id  = azurerm_key_vault.fg-keyvault.id
+  key_vault_id  = azurerm_key_vault.ok-keyvault.id
   tenant_id     = "31673592-0635-48d1-a494-9887ca994b24"
   object_id     = azurerm_linux_web_app.frontend-webapp.identity[0].principal_id
 
@@ -60,9 +60,9 @@ resource "azurerm_key_vault_access_policy" "kv_access_policy_web_app" {
 
 resource "azurerm_monitor_diagnostic_setting" "kvlog" {
   name                       = "kv-log-diagonise"
-  target_resource_id         = azurerm_key_vault.fg-keyvault.id
+  target_resource_id         = azurerm_key_vault.ok-keyvault.id
   storage_account_id         = azurerm_storage_account.fn-storageaccount.id
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.fg-loganalytics.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.ok-loganalytics.id
 
 
   enabled_log {
