9d58f706c87a72d9c340d915f2bf44975c25d729
OklenCodes <oklen.blake@hotmail.co.uk>
2024-09-14 19:32:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
   tags = {
-    Enviroment = "Task 1"
-    Team       = "Cloud"
+    Environment = "Cloud Team Task 1"
   }
 }
